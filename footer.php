e1f456eea6cc1973c41fb2d257ab946354023b76
jehorehlabrador <156505719+jehorehlabrador@users.noreply.github.com>
2025-08-02 08:34:52 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!-- includes/footer.php -->
 <footer>
-    <p>&copy; 2025 Saving Strays. All rights reserved.</p>
+    <p>&copy; 2025 Saving strays. All rights reserved.</p>
 </footer>
 </body>
 </html>
